3ac26aa758da73ca5eaf87226e88e4172a48cf22
Thiago Tobias <thiagotobias@gmail.com>
2023-06-10 14:44:39 -0300

    Project created

---
+++ src/main/java/br/com/saibot/dungeonAndDragons/DungeonAndDragonsBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.saibot.dungeonAndDragons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonAndDragonsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonAndDragonsBackendApplication.class, args);
+	}
+
+}
